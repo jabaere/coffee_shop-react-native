ce771449c78f2ec9860d735affe5fdbcf3f9b8eb
jaba jaba <frosky.ge@gmail.com>
2023-07-22 12:33:19 +0400

    add userui

--- screens/HomePage.jsx
+++ screens/HomePage.jsx
@@ -1,18 +1,70 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CoffeeCard from "../components/CoffeeCard";
 import { coffeeInfo } from "../utils/globalFuntions";
-import { SimpleLineIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import Glass from "../assets/svg/Glass.svg";
 import GlassFill from "../assets/svg/GlassFill.svg";
 export function HomePage() {
+  //detect period
+
+  function getDayPeriod(currentHour) {
+    // const currentHour = new Date().getHours();
+
+    if (currentHour >= 5 && currentHour < 12) {
+      return "morning";
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return "afternoon";
+    } else if (currentHour >= 18 && currentHour < 21) {
+      return "evening";
+    } else {
+      return "night";
+    }
+  }
+
+  const [dayPeriod, setDayPeriod] = useState("");
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    const period = getDayPeriod(currentHour);
+    setDayPeriod(period);
+  }, []);
+
+  //
+  const [loyalty, setLoyalty] = useState({
+    total: 8,
+    userOwn: 4,
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.user}></View>
+      <View style={styles.user}>
+        <View style={styles.greeting}>
+          <Text style={styles.user__greeting}>Good {dayPeriod}</Text>
+          <Text style={styles.user__name}>User</Text>
+        </View>
+        <View style={styles.icons}>
+          <TouchableOpacity
+            onPress={() => {
+              /*navigate shopping cart*/
+            }}
+          >
+            <AntDesign name="shoppingcart" size={26} color="#001833" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              /*navigate shopping profile*/
+            }}
+          >
+            <AntDesign name="user" size={26} color="#001833" />
+          </TouchableOpacity>
+        </View>
+      </View>
       <View style={styles.loyalty}>
         <View style={styles.textContainerLoyalty}>
           <Text style={styles.title}>Loyalty card</Text>
-          <Text style={styles.title}>4 / 8</Text>
+          <Text style={styles.title}>
+            {loyalty.userOwn} / {loyalty.total}
+          </Text>
         </View>
         <View style={styles.glassContainer}>
           {Array.from({ length: 8 }).map((item, index) =>
@@ -49,7 +101,28 @@ const styles = StyleSheet.create({
   },
   user: {
     height: 100,
-    backgroundColor: "red",
+    width: 300,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingTop: 29,
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  greeting: {},
+  user__greeting: {
+    color: "#D8D8D8",
+    fontSize: 14,
+    fontFamily: "Poppins_500Medium",
+  },
+
+  user__name: {
+    fontFamily: "Poppins_500Medium",
+    color: "#001833",
+    fontSize: 18,
+  },
+  icons: {
+    flexDirection: "row",
+    gap: 20,
   },
   loyalty: {
     height: 122,
